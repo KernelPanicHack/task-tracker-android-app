4790a32268b79d85206632b2934b921baacd28d8
Abdulali-coll <boomdoom22338@gmail.com>
2024-10-26 03:31:32 +0300

    create popup menu

---
+++ app/src/main/java/com/example/hackaton_project/BoardActivity.kt
@@ -0,0 +1,56 @@
+package com.example.hackaton_project
+
+import android.os.Bundle
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageButton
+import android.widget.PopupMenu
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_board)
+
+        // Настройка системных отступов
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.boardid)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val profileBut: ImageButton = findViewById(R.id.ProfileId)
+        profileBut.setOnClickListener { showPopupMenu(it) }
+    }
+
+    private fun showPopupMenu(view: View) {
+        val popup = PopupMenu(this, view)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.popup_menu, popup.menu)
+
+        popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.action_settings -> {
+                    // Обработка нажатия на пункт "Settings"
+                    true
+                }
+                R.id.action_profile -> {
+                    // Обработка нажатия на пункт "Profile"
+                    true
+                }
+                R.id.action_logout -> {
+                    // Обработка нажатия на пункт "Logout"
+                    true
+                }
+                else -> false
+            }
+        }
+
+        popup.show()
+    }
+}
